198fd43a7c2bf8c4493b7eb6177819f04f7a86db
Bart van Pelt <bart.vanpelt@gmail.com>
2026-05-01 19:55:57 +0200

    First commit

---
+++ src/test/java/com/bsoft/timeseries/TimeseriesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bsoft.timeseries;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimeseriesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
